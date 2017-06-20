58bc7885893e6277692f5b9fb4c10385b1437ec1
Apurva Nitanjay <apuayush@gmail.com>
2017-06-20 15:31:32 +0530

    login comments added

--- login.py
+++ login.py
@@ -25,6 +25,16 @@ def login(username="", password=""):
 
     br.method = "POST"
     response = br.submit()
-    # print response
+    # print response.read
     return br
 
+# how to use
+# from login import login
+# browser = login(username,password)  use this for first time login
+# html_page = browser.login("https://www.codechef.com/node")
+# soup = BeautifulSoup(html_page) and then further scrapping
+if __name__ == "__main__":
+    br = login('apuayush','Apurvanit@2304')
+    afterlogin = br.open("https://www.codechef.com/node")
+    print afterlogin.read()
+    print br.geturl()
